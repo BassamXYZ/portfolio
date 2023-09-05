db117d36eb3af3255c41e166a2fd2cf1d9ece439
bassamxyz <beaxyzxyzxyz@gmail.com>
2023-09-05 08:44:50 +0300

    __

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
@@ -15,5 +16,11 @@ export default defineConfig({
         en: 'en-US',
       },
     },
-  }),]
+  }),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
 });
